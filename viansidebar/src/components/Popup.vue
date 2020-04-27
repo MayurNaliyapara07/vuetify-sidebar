220db36276908be30a04b66ceb17abf1630b3a35
Vian A. Noori <32823002+vianadeeb@users.noreply.github.com>
2020-04-27 21:05:23 +0300

    v1

---
+++ viansidebar/src/components/Popup.vue
@@ -0,0 +1,20 @@
+<template>
+  <v-dialog max-width="600px">
+    <v-btn flat slot="activator" class="success">Add New order</v-btn>
+    <v-card>
+      <v-card-title>
+        <h2>Add a New order</h2>
+      </v-card-title>
+    </v-card>
+  </v-dialog>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+
+    }
+  }
+}
+</script>
